11c91fe300a2f459e33da677de9e2f91160465ae
Enes <70810472+ahmetenesdur@users.noreply.github.com>
2023-01-05 21:13:25 +0300

    Added Disconnect

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,191 +1,207 @@
 import React from "react";
 import Image from "next/image";
 import { Web3Button } from "@web3modal/react";
-import { useAccount } from "wagmi";
+import { useAccount, useDisconnect } from "wagmi";
 import {
-    Box,
-    Flex,
-    HStack,
-    IconButton,
-    Link,
-    useColorModeValue,
-    useColorMode,
-    Button,
-    useDisclosure,
-    Stack,
+  Box,
+  Flex,
+  HStack,
+  IconButton,
+  Link,
+  useColorModeValue,
+  useColorMode,
+  Button,
+  useDisclosure,
+  Stack,
 } from "@chakra-ui/react";
 import {
-    AddIcon,
-    CloseIcon,
-    HamburgerIcon,
-    MoonIcon,
-    SunIcon,
+  AddIcon,
+  CloseIcon,
+  HamburgerIcon,
+  MoonIcon,
+  SunIcon,
 } from "@chakra-ui/icons";
 
 import ConfessionModal from "./ConfessionModal";
 
 const links = [
-    { label: "My Confessions", to: "/myconfessions" },
-    { label: "Coffees", to: "/coffees" },
+  { label: "My Confessions", to: "/myconfessions" },
+  { label: "Coffees", to: "/coffees" },
 ];
 
 const NavLink = ({ children, to }) => (
-    <Link
-        px={2}
-        py={1}
-        rounded={"md"}
-        color={useColorModeValue("gray.800", "gray.200")}
-        _hover={{
-            textDecoration: "none",
-            color: useColorModeValue("gray.600", "gray.400"),
-        }}
-        href={to}
-    >
-        {children}
-    </Link>
+  <Link
+    px={2}
+    py={1}
+    rounded={"md"}
+    color={useColorModeValue("gray.800", "gray.200")}
+    _hover={{
+      textDecoration: "none",
+      color: useColorModeValue("gray.600", "gray.400"),
+    }}
+    href={to}
+  >
+    {children}
+  </Link>
 );
 
 const Layout = ({ children }) => {
-    const { colorMode, toggleColorMode } = useColorMode();
-    const { isOpen, onOpen, onClose } = useDisclosure();
-    const { address, isConnected } = useAccount();
+  const { colorMode, toggleColorMode } = useColorMode();
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const { address, isConnected } = useAccount();
 
-    const {
-        isOpen: isModalOpen,
-        onOpen: onModalOpen,
-        onClose: onModalClose,
-    } = useDisclosure();
+  const { disconnect } = useDisconnect();
 
-    return (
-        <Box
-            h="100vh"
-            w="100vw"
-            position="relative"
-            zIndex="0"
-            bg={useColorModeValue("white", "#171923")}
-        >
-            <Box px={4} maxWidth={"5xl"} m={"auto"} mt={"5"}>
-                <Flex h={"16"} alignItems={"center"} justifyContent={"space-between"}>
-                    <HStack spacing={8} alignItems={"center"}>
-                        <Box>
-                            <Link href={"/"}>
-                                <Image
-                                    src={"/confesster.svg"}
-                                    alt={"Confesster"}
-                                    width={"120"}
-                                    height={"120"}
-                                />
-                            </Link>
-                        </Box>
+  const {
+    isOpen: isModalOpen,
+    onOpen: onModalOpen,
+    onClose: onModalClose,
+  } = useDisclosure();
 
-                        <HStack
-                            as={"nav"}
-                            spacing={4}
-                            display={{
-                                base: "none",
-                                md: "flex",
-                            }}
-                        >
-                            {links.map(({ label, to }) => (
-                                <NavLink key={label} to={to}>
-                                    {label}
-                                </NavLink>
-                            ))}
-                        </HStack>
-                    </HStack>
-
-                    <Flex
-                        alignItems={"center"}
-                        display={{
-                            base: "none",
-                            md: "flex",
-                        }}
-                    >
-                        <IconButton
-                            size={"md"}
-                            icon={colorMode === "light" ? <MoonIcon /> : <SunIcon />}
-                            aria-label={"Toggle Color Mode"}
-                            onClick={toggleColorMode}
-                            mr={4}
-                        />
-
-                        {isConnected && (
-                            <Button
-                                variant={"solid"}
-                                colorScheme={"purple"}
-                                size={"sm"}
-                                mr={4}
-                                leftIcon={<AddIcon />}
-                                onClick={onModalOpen}
-                            >
-                                Confess
-                            </Button>
-                        )}
-
-                        {isConnected ? (
-                            <Button variant={"outline"} colorScheme={"purple"} size={"sm"}>
-                                {address?.slice(0, 6)}...{address?.slice(-4)}
-                            </Button>
-                        ) : (
-                            <Web3Button />
-                        )}
-                    </Flex>
-                    <IconButton
-                        size={"md"}
-                        icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
-                        aria-label={"Open Menu"}
-                        display={{ md: "none" }}
-                        onClick={isOpen ? onClose : onOpen}
-                    />
-                </Flex>
-                {isOpen ? (
-                    <Box pb={4} display={{ md: "none" }}>
-                        <Stack as={"nav"} spacing={4}>
-                            {links.map(({ label, to }) => (
-                                <NavLink key={label} to={to}>
-                                    {label}
-                                </NavLink>
-                            ))}
-                            <IconButton
-                                size={"md"}
-                                icon={colorMode === "light" ? <MoonIcon /> : <SunIcon />}
-                                aria-label={"Toggle Color Mode"}
-                                onClick={toggleColorMode}
-                                mr={4}
-                            />
-
-                            {isConnected && (
-                                <Button
-                                    variant={"solid"}
-                                    colorScheme={"purple"}
-                                    size={"sm"}
-                                    mr={4}
-                                    leftIcon={<AddIcon />}
-                                    onClick={() => { }}
-                                >
-                                    Confess
-                                </Button>
-                            )}
-
-                            {isConnected ? (
-                                <Button variant={"outline"} colorScheme={"purple"} size={"sm"}>
-                                    {address?.slice(0, 6)}...{address?.slice(-4)}
-                                </Button>
-                            ) : (
-                                <Web3Button />
-                            )}
-                        </Stack>
-                    </Box>
-                ) : null}
+  return (
+    <Box
+      h="100vh"
+      w="100vw"
+      position="relative"
+      zIndex="0"
+      bg={useColorModeValue("white", "#171923")}
+    >
+      <Box px={4} maxWidth={"5xl"} m={"auto"} mt={"5"}>
+        <Flex h={"16"} alignItems={"center"} justifyContent={"space-between"}>
+          <HStack spacing={8} alignItems={"center"}>
+            <Box>
+              <Link href={"/"}>
+                <Image
+                  src={"/confesster.svg"}
+                  alt={"Confesster"}
+                  width={"120"}
+                  height={"120"}
+                />
+              </Link>
             </Box>
 
-            <Box px={4} maxWidth={"5xl"} m={"auto"} mt={"5"}>
-                {children}
-            </Box>
+            <HStack
+              as={"nav"}
+              spacing={4}
+              display={{
+                base: "none",
+                md: "flex",
+              }}
+            >
+              {links.map(({ label, to }) => (
+                <NavLink key={label} to={to}>
+                  {label}
+                </NavLink>
+              ))}
+            </HStack>
+          </HStack>
 
-            <ConfessionModal isOpen={isModalOpen} onClose={onModalClose} />
-        </Box>
-    );
+          <Flex
+            alignItems={"center"}
+            display={{
+              base: "none",
+              md: "flex",
+            }}
+          >
+            <IconButton
+              size={"md"}
+              icon={colorMode === "light" ? <MoonIcon /> : <SunIcon />}
+              aria-label={"Toggle Color Mode"}
+              onClick={toggleColorMode}
+              mr={4}
+            />
+
+            {isConnected && (
+              <Button
+                variant={"solid"}
+                colorScheme={"purple"}
+                size={"sm"}
+                mr={4}
+                leftIcon={<AddIcon />}
+                onClick={onModalOpen}
+              >
+                Confess
+              </Button>
+            )}
+
+            {isConnected ? (
+              <Button
+                variant={"outline"}
+                colorScheme={"purple"}
+                size={"sm"}
+                onClick={() => {
+                  disconnect();
+                }}
+              >
+                {address?.slice(0, 6)}...{address?.slice(-4)}
+              </Button>
+            ) : (
+              <Web3Button />
+            )}
+          </Flex>
+          <IconButton
+            size={"md"}
+            icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
+            aria-label={"Open Menu"}
+            display={{ md: "none" }}
+            onClick={isOpen ? onClose : onOpen}
+          />
+        </Flex>
+        {isOpen ? (
+          <Box pb={4} display={{ md: "none" }}>
+            <Stack as={"nav"} spacing={4}>
+              {links.map(({ label, to }) => (
+                <NavLink key={label} to={to}>
+                  {label}
+                </NavLink>
+              ))}
+              <IconButton
+                size={"md"}
+                icon={colorMode === "light" ? <MoonIcon /> : <SunIcon />}
+                aria-label={"Toggle Color Mode"}
+                onClick={toggleColorMode}
+                mr={4}
+              />
+
+              {isConnected && (
+                <Button
+                  variant={"solid"}
+                  colorScheme={"purple"}
+                  size={"sm"}
+                  mr={4}
+                  leftIcon={<AddIcon />}
+                  onClick={() => {}}
+                >
+                  Confess
+                </Button>
+              )}
+
+              {isConnected ? (
+                <Button
+                  variant={"outline"}
+                  colorScheme={"purple"}
+                  size={"sm"}
+                  onClick={() => {
+                    disconnect();
+                  }}
+                >
+                  {address?.slice(0, 6)}...{address?.slice(-4)}
+                </Button>
+              ) : (
+                <Web3Button />
+              )}
+            </Stack>
+          </Box>
+        ) : null}
+      </Box>
+
+      <Box px={4} maxWidth={"5xl"} m={"auto"} mt={"5"}>
+        {children}
+      </Box>
+
+      <ConfessionModal isOpen={isModalOpen} onClose={onModalClose} />
+    </Box>
+  );
 };
 
 export default Layout;
