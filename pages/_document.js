20f100457c5beff10249e1ced37610e5bb4dc806
Enes <70810472+ahmetenesdur@users.noreply.github.com>
2023-01-05 19:35:50 +0300

    Added buymeacoffee Widget

--- pages/_document.js
+++ pages/_document.js
@@ -12,9 +12,9 @@ export default function Document() {
           async
           data-name="BMC-Widget"
           data-cfasync="false"
-          src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js" data-id="asmalsosyal"
+          src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js" data-id="ahmetenesdur"
           data-description="Support me on Buy me a coffee!"
-          data-message="Enjoy yourself."
+          data-message="Thanks for the coffee."
           data-color="#5F7FFF"
           data-position="Right"
           data-x_margin="18"
